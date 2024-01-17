4facd457071497339a44d457a23d860dfe90e7d3
paul <cebu13@gmail.com>
2024-01-17 17:21:56 +0900

    google login popup #2

--- client_petcong/lib/pages/homepage.dart
+++ client_petcong/lib/pages/homepage.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:petcong/assets/widgets/navigations.dart';
+import 'package:petcong/controller/user_controller.dart';
+import 'package:petcong/pages/signin_pages/sign_in_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -38,6 +40,21 @@ class _HomePageState extends State<HomePage> {
               },
             ),
           ),
+          CircleAvatar(
+              foregroundImage:
+                  NetworkImage(UserController.user?.photoURL ?? ''),
+            ),
+            Text(UserController.user?.displayName ?? ''),
+            ElevatedButton(
+                onPressed: () async {
+                  await UserController.signOut();
+                  if (mounted) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => const SignInPage(),
+                    ));
+                  }
+                },
+                child: const Text("Logout"))
         ],
       ),
     );
--- client_petcong/lib/pages/signin_pages/sign_in_page.dart
+++ client_petcong/lib/pages/signin_pages/sign_in_page.dart
@@ -1,34 +1,54 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:petcong/controller/user_controller.dart';
+
 import 'package:petcong/pages/homepage.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
 
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Google SignIn"),
       ),
-      body: Center(
-        child: SizedBox(
-          height: 50,
-          child: GestureDetector(
-            onTap: _handleGoogleSignIn,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: const Center(
-                child: Text(
-                  "Sign up with Google",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: SafeArea(
+        minimum: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            children: [_googleSignInButton()],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _googleSignInButton() {
+    return Center(
+      child: SizedBox(
+        height: 50,
+        child: GestureDetector(
+          onTap: _handleGoogleSignIn,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Center(
+              child: Text(
+                "Sign up with Google",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
@@ -38,33 +58,42 @@ class SignInPage extends StatelessWidget {
     );
   }
 
-  // void _handleGoogleSignIn() {
-  //   try {
-  //     GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
-  //     FirebaseAuth.instance.signInWithProvider(googleAuthProvider);
-  //   } catch (error) {
-  //     print(error);
-  //   }
-  // }
   void _handleGoogleSignIn() async {
-    try {
-      final user = await UserController.loginWithGoogle();
-      if (user != null && mounted) {
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => const HomePage()));
+    if (!Platform.isAndroid) {
+      try {
+        GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
+        FirebaseAuth.instance.signInWithProvider(googleAuthProvider);
+      } catch (error) {
+        print(error);
       }
-    } on FirebaseAuthException catch (error) {
-      print(error.message);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(
-        error.message ?? "Something went wrong",
-      )));
-    } catch (error) {
-      print(error);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(
-        error.toString(),
-      )));
+    } else {
+      try {
+        final user = await UserController.loginWithGoogle();
+        print(user);
+        if (user != null && mounted) {
+          print(mounted);
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => const HomePage()));
+        }
+      } on FirebaseAuthException catch (error) {
+        print(error.message);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              error.message ?? "Something went wrong",
+            ),
+          ),
+        );
+      } catch (error) {
+        print(error);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              error.toString(),
+            ),
+          ),
+        );
+      }
     }
   }
 }
