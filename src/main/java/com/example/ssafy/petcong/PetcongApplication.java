200de896ffcf21ead2100247a1ac9d983abaf585
신문영 <ztrl@naver.com>
2024-01-17 14:30:34 +0900

    init commit

---
+++ src/main/java/com/example/ssafy/petcong/PetcongApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssafy.petcong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetcongApplication.class, args);
+    }
+
+}
