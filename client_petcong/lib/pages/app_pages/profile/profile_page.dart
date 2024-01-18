7ffece3404824a7b0987f18a4f3d2af1f7d76689
youngjoo <youngjoo000108@gmail.com>
2024-01-18 10:23:27 +0900

    0118morning

--- client_petcong/lib/pages/app_pages/profile/profile_page.dart
+++ client_petcong/lib/pages/app_pages/profile/profile_page.dart
@@ -1,34 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:petcong/main.dart';
-import 'package:petcong/pages/signin_pages/sign_in_page.dart';
+import 'package:petcong/pages/app_pages/profile/pet_name_page.dart';
 
-class MainProfilePage extends StatefulWidget {
+class MainProfilePage extends StatelessWidget {
   const MainProfilePage({Key? key}) : super(key: key);
-
-  @override
-  State<MainProfilePage> createState() => _MainProfilePageState();
-}
-
-class _MainProfilePageState extends State<MainProfilePage> {
-  Future<void> signOut(BuildContext context) async {
-    try {
-      print(mounted);
-      await FirebaseAuth.instance.signOut();
-      print("sign out");
-      print(mounted);
-      if (mounted) {
-        print("sign out mounted");
-        Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => const SignInPage()),
-          (route) => false,
-        );
-      }
-    } catch (error) {
-      debugPrint('sign out failed $error');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,18 +11,17 @@ class _MainProfilePageState extends State<MainProfilePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("프로필 페이지"),
+            const Text("Profile Page"),
             const SizedBox(height: 30),
             ElevatedButton(
-              onPressed: () => {signOut(context)},
-              // {
-              //   // Navigator.push(
-              //   //   context,
-              //   //   MaterialPageRoute(builder: (context) => const MyAppContent()),
-              //   // );
-              // },
-              child: const Text("로그아웃"),
-            ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PetNamePage()),
+                  );
+                },
+                child: const Text('Go to write profile')),
           ],
         ),
       ),
