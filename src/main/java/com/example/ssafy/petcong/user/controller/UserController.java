5309bc15bf4457a85fec7774c6db2a852052718a
신문영 <ztrl@naver.com>
2024-01-18 17:40:47 +0900

    feat: Add temporal signup/signin method to controller

--- src/main/java/com/example/ssafy/petcong/user/controller/UserController.java
+++ src/main/java/com/example/ssafy/petcong/user/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.example.ssafy.petcong.user.controller;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.*;
+import com.google.firebase.auth.internal.FirebaseCustomAuthToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-public class UserController { }
+public class UserController {
+    private final FirebaseAuth firebaseAuth;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(String gmail) throws FirebaseAuthException {
+        UserRecord userRecord = firebaseAuth.getUserByEmail(gmail);
+        return ResponseEntity.ok().body(userRecord);
+    }
+    @PostMapping("/signin")
+    public ResponseEntity<String> signin(String userUid, String idToken) throws FirebaseAuthException {
+        FirebaseToken decodedToken = firebaseAuth.verifyIdToken(idToken);
+
+        if (userUid != null && decodedToken != null) {
+            if (userUid.equals(decodedToken.getUid())) {
+                return ResponseEntity.ok().body("ok");
+            }
+        }
+        
+        return ResponseEntity.badRequest().body("this is test response");
+    }
+}
