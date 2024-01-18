a38e38d91de0d1ef87f343ed82045d48989bd188
jongwoo <jongwoop97@gmail.com>
2024-01-18 15:16:39 +0900

    feat: MatchingProfileService 매칭 메인 로직 구현
    온라인 유저들을 담은 공통 큐에서 유저를 가져와, 추천 가능한
    유저인지 확인후 반환
    todo: 필터 로직, 큐 관리 로직, 리파지토리에서 유저 정보 가져오기

--- src/main/java/com/example/ssafy/petcong/matching/service/MatchingProfileServiceImpl.java
+++ src/main/java/com/example/ssafy/petcong/matching/service/MatchingProfileServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.ssafy.petcong.matching.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -8,16 +9,42 @@ import java.util.Map;
 @Service
 public class MatchingProfileServiceImpl implements MatchingProfileService {
 
+    private final OnlineUsersService onlineUsers;
+
+    @Autowired
+    public MatchingProfileServiceImpl(OnlineUsersService onlineUsers) {
+        this.onlineUsers = onlineUsers;
+    }
+
     public Map<String, Object> details(String userId) {
-        int onlineUserCnt = 0;
+        String filteredUser = null;
+        for (int i = 0; i < onlineUsers.sizeOfQueue(); i++) {
+            String potentialUser = nextOnlineUser();
+            if (potentialUser.equals("empty")) {
+                break;
+            } else if (isPotentialUser(userId, potentialUser)) {
+                filteredUser = potentialUser;
+                break;
+            }
+        }
 
         Map<String, Object> res = new HashMap<>();
+        res.put("potentialUser", filteredUser);
         return res;
     }
 
-    public String nextOnlineUser() {
+    private String nextOnlineUser() {
         // if linkedblockingqueue is empty, return "empty"
+        if (onlineUsers.sizeOfQueue() == 0) {
+            return "empty";
+        }
+        // to-do: get next online user
         return "userid";
+    }
+
+    private boolean isPotentialUser(String toUser, String recUser) {
+        // implement filter to find if potential user is available
+        return true;
     }
 
 }
