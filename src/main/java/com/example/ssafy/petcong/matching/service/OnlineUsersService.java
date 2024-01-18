04d466c4288a66f891f01a407c8b5488bd6d44c1
jongwoo <jongwoop97@gmail.com>
2024-01-18 15:16:05 +0900

    refactor: OnlineUsersService Interface에 공통 구현 요구 메서드 추가

--- src/main/java/com/example/ssafy/petcong/matching/service/OnlineUsersService.java
+++ src/main/java/com/example/ssafy/petcong/matching/service/OnlineUsersService.java
@@ -1,4 +1,9 @@
 package com.example.ssafy.petcong.matching.service;
 
 public interface OnlineUsersService {
+    void addUserToQueue(int userId);
+
+    int removeUserFromQueue();
+
+    int sizeOfQueue();
 }
