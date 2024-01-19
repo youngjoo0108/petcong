5c9081012a90a20bdab2aec97639be07f50c1b3e
youngjoo <youngjoo000108@gmail.com>
2024-01-19 10:49:52 +0900

    print('successssssssssssssssssssssssssssssssssss')

--- client_petcong/lib/main.dart
+++ client_petcong/lib/main.dart
@@ -1,16 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:petcong/firebase_options.dart';
-import 'package:petcong/pages/homepage.dart';
 import 'package:petcong/pages/signin_pages/sign_in_page.dart';
-import 'package:petcong/pages/signin_pages/splash_screen_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -20,35 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyAppContent(),
+      home: SignInPage(),
     );
-  }
-}
-
-class MyAppContent extends StatefulWidget {
-  const MyAppContent({Key? key}) : super(key: key);
-
-  @override
-  State<MyAppContent> createState() => _MyAppContentState();
-}
-
-class _MyAppContentState extends State<MyAppContent> {
-  User? _user;
-
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen(
-      child: _user != null ? const HomePage() : const SignInPage(),
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    FirebaseAuth.instance.authStateChanges().listen((user) {
-      setState(() {
-        _user = user;
-      });
-    });
   }
 }
--- client_petcong/lib/pages/app_pages/profile/profile_page.dart
+++ client_petcong/lib/pages/app_pages/profile/profile_page.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:petcong/controller/user_controller.dart';
 import 'package:petcong/pages/app_pages/profile/pet_name_page.dart';
+import 'package:petcong/pages/signin_pages/sign_in_page.dart';
 
 class MainProfilePage extends StatelessWidget {
   const MainProfilePage({Key? key}) : super(key: key);
@@ -22,6 +25,16 @@ class MainProfilePage extends StatelessWidget {
                   );
                 },
                 child: const Text('Go to write profile')),
+            ElevatedButton(
+                onPressed: () {
+                  UserController.signOut();
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignInPage()),
+                      (route) => false);
+                },
+                child: const Text('log out')),
           ],
         ),
       ),
