3dc9106ee7b921cd1a54e2cc4edd2b1ba39bf400
신문영 <ztrl@naver.com>
2024-01-19 17:34:15 +0900

    fix: Fix UserController to test signin Firebase authentication is independent environment, so we cannot test authentication process in Spring boot test environment.

--- src/main/java/com/example/ssafy/petcong/user/controller/UserController.java
+++ src/main/java/com/example/ssafy/petcong/user/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.example.ssafy.petcong.user.controller;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.*;
-import com.google.firebase.auth.internal.FirebaseCustomAuthToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,24 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class UserController {
-    private final FirebaseAuth firebaseAuth;
-
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(String gmail) throws FirebaseAuthException {
-        UserRecord userRecord = firebaseAuth.getUserByEmail(gmail);
-        return ResponseEntity.ok().body(userRecord);
-    }
     @PostMapping("/signin")
-    public ResponseEntity<String> signin(String userUid, String idToken) throws FirebaseAuthException {
-        FirebaseToken decodedToken = firebaseAuth.verifyIdToken(idToken);
-
-        if (userUid != null && decodedToken != null) {
-            if (userUid.equals(decodedToken.getUid())) {
-                return ResponseEntity.ok().body("ok");
-            }
-        }
-        
-        return ResponseEntity.badRequest().body("this is test response");
+    public ResponseEntity<String> signin() {
+        log.info("isAuthenticated ? : " + SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
+        return ResponseEntity.ok().body("success");
     }
 }
