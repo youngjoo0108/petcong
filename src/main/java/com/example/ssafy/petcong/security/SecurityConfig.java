00e344d8fb65c3952b14bfc01a68f1ea35779e16
신문영 <ztrl@naver.com>
2024-01-19 17:29:27 +0900

    feat: Add SecurityConfig Complete SecurityFilterChain

--- src/main/java/com/example/ssafy/petcong/security/SecurityConfig.java
+++ src/main/java/com/example/ssafy/petcong/security/SecurityConfig.java
@@ -1,38 +1,35 @@
 package com.example.ssafy.petcong.security;
 
-import com.example.ssafy.petcong.security.filter.FirebaseTokenFilter;
-
-import com.google.firebase.auth.FirebaseAuth;
-
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-    private final UserDetailsService userDetailsService;
-    private final FirebaseAuth firebaseAuth;
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager firebaseAuthenticationManager(AuthenticationProvider firebaseAuthenticationProvider) {
+        return new ProviderManager(firebaseAuthenticationProvider);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager firebaseAuthenticationManager) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests((requests)
-                    -> requests
-                    .requestMatchers("/users/signin", "/users/signup").permitAll()
+            .addFilterBefore(new FirebaseAuthenticationFilter(firebaseAuthenticationManager), UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests(requests -> requests
                     .anyRequest().authenticated())
-            .addFilterBefore(new FirebaseTokenFilter(firebaseAuth, userDetailsService), UsernamePasswordAuthenticationFilter.class)
-            .exceptionHandling(configurer -> configurer.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+            .exceptionHandling(configurer -> configurer
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
         return http.build();
     }
 }
