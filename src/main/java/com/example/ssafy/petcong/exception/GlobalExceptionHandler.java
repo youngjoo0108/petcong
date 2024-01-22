e3e79e34afcc9c41b354d520a526f8d59ba8a870
신문영 <ztrl@naver.com>
2024-01-22 15:39:24 +0900

    fix: Fix handleMethodArgumentNotValidException
    MethodArgumentNotValidException is spring mvc exception

--- src/main/java/com/example/ssafy/petcong/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ssafy/petcong/exception/GlobalExceptionHandler.java
@@ -2,21 +2,24 @@ package com.example.ssafy.petcong.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
-        log.warn(methodArgumentNotValidException.getMessage());
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn(ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(methodArgumentNotValidException.getMessage());
+                .body(ex.getMessage());
     }
 }
