73c84997ecd0e48d2054456768872178f04a5120
신문영 <ztrl@naver.com>
2024-01-22 18:05:54 +0900

    feat: Add uploadImage to UserService

--- src/main/java/com/example/ssafy/petcong/user/service/UserService.java
+++ src/main/java/com/example/ssafy/petcong/user/service/UserService.java
@@ -1,10 +1,15 @@
 package com.example.ssafy.petcong.user.service;
 
+import com.example.ssafy.petcong.user.model.record.UserImgRecord;
 import com.example.ssafy.petcong.user.model.record.UserRecord;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface UserService {
     UserRecord findUserByUserId(int userId);
     UserRecord findUserByUid(String uid);
     UserRecord save(UserRecord userRecord);
     UserRecord updateCallable(UserRecord userRecord, boolean state);
+    UserImgRecord uploadImage(UserImgRecord userImgRecord,  MultipartFile file) throws IOException;
 }
--- src/main/java/com/example/ssafy/petcong/user/service/UserServiceImpl.java
+++ src/main/java/com/example/ssafy/petcong/user/service/UserServiceImpl.java
@@ -1,6 +1,12 @@
 package com.example.ssafy.petcong.user.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.example.ssafy.petcong.user.model.entity.User;
+import com.example.ssafy.petcong.user.model.record.UserImgRecord;
 import com.example.ssafy.petcong.user.model.record.UserRecord;
 import com.example.ssafy.petcong.user.repository.UserRepository;
 
@@ -9,11 +15,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final Bucket bucket;
+    private final AmazonS3 s3Client;
     private final UserRepository userRepository;
 
     @Override
@@ -36,5 +48,27 @@ public class UserServiceImpl implements UserService {
     public UserRecord updateCallable(UserRecord userRecord, boolean state) {
         User userEntity = new User(userRecord).updateCallable(state);
         return userRepository.save(userEntity);
+    }
+
+    @Override
+    public UserImgRecord uploadImage(UserImgRecord userImgRecord, MultipartFile file) throws IOException {
+        try (InputStream fileInputStream = file.getInputStream()) {
+            String contentType = file.getContentType();
+            String originalFileName = file.getOriginalFilename();
+            Long size = file.getSize();
+            log.info("ContentType: " + contentType);
+            log.info("OriginalFileName: " + originalFileName);
+            log.info("Size: " + size);
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(contentType);
+            metadata.setContentLength(size);
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket.getName(), originalFileName, fileInputStream, metadata);
+
+            PutObjectResult putObjectResult = s3Client.putObject(putObjectRequest);
+            boolean uploadResult = putObjectResult.getMetadata().getBucketKeyEnabled();
+        }
+        return null;
     }
 }
