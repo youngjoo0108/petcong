65336f8877a0bedbb59213e3151b367cc0df5891
Ehigh <rkd2rb@naver.com>
2024-01-23 23:48:50 +0900

    test: 매칭 컨트롤러 테스트 클래스 생성

---
+++ src/test/java/com/example/ssafy/petcong/matching/controller/MatchingControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.ssafy.petcong.matching.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchingControllerTest {
+
+    /*
+    choice
+     */
+    // 1. 정상 요청 - pending
+
+    // 2. 정상 요청 - matched
+
+    // 3. 비정상 요청 - rejected
+
+    // 4. 비정상 요청 - 이미 matched
+
+    // 5. 비정상 요청 - from me to me
+
+    // 6. 비정상 요청 - toUserId == null
+
+    /*
+    onCallEnd
+     */
+
+}
