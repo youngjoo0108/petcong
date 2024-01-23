35bc27cb8b405545e0139b3b6ae7d07a42e1d90e
youngjoo <youngjoo000108@gmail.com>
2024-01-23 12:57:32 +0900

    add webrtc

--- client_petcong/lib/pages/app_pages/matching/matching_page.dart
+++ client_petcong/lib/pages/app_pages/matching/matching_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:petcong/pages/app_pages/webRTC/webrtc.dart';
 
 class MainMatchingPage extends StatelessWidget {
   const MainMatchingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Matching Page"),
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [
+          const Text("Matching Page"),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MainVideoCall(),
+                  ),
+                );
+              },
+              child: const Text('Call'))
+        ],
       ),
-    );
+    ));
   }
 }
