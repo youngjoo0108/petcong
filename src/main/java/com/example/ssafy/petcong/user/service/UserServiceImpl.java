402382f57ccd1ead54c38efc37538a398aa7d0f0
신문영 <ztrl@naver.com>
2024-01-24 18:08:29 +0900

    feat: Add service logic Add createPresignedUrlForGetImage()

--- src/main/java/com/example/ssafy/petcong/user/service/UserServiceImpl.java
+++ src/main/java/com/example/ssafy/petcong/user/service/UserServiceImpl.java
@@ -1,74 +1,119 @@
 package com.example.ssafy.petcong.user.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
 import com.example.ssafy.petcong.user.model.entity.User;
+import com.example.ssafy.petcong.user.model.entity.UserImg;
 import com.example.ssafy.petcong.user.model.record.UserImgRecord;
 import com.example.ssafy.petcong.user.model.record.UserRecord;
+import com.example.ssafy.petcong.user.repository.UserImgRepository;
 import com.example.ssafy.petcong.user.repository.UserRepository;
 
-import lombok.RequiredArgsConstructor;
+import jakarta.transaction.Transactional;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Duration;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final Bucket bucket;
-    private final AmazonS3 s3Client;
+    private final S3Client s3Client;
     private final UserRepository userRepository;
-
-    @Override
-    public UserRecord findUserByUserId(int userId) {
-        return userRepository.findUserByUserId(userId);
-    }
+    private final UserImgRepository userImgRepository;
 
     @Override
     public UserRecord findUserByUid(String uid) {
-        return userRepository.findUserByUid(uid);
+        User result = userRepository.findUserByUid(uid);
+        UserRecord record = new UserRecord(result);
+        return record;
     }
 
     @Override
     public UserRecord save(UserRecord userRecord) {
         User userEntity = new User(userRecord);
-        return userRepository.save(userEntity);
+        User result = userRepository.save(userEntity);
+        UserRecord record = new UserRecord(result);
+        return record;
     }
 
     @Override
     public UserRecord updateCallable(UserRecord userRecord, boolean state) {
         User userEntity = new User(userRecord).updateCallable(state);
-        return userRepository.save(userEntity);
+        User result = userRepository.save(userEntity);
+        UserRecord record = new UserRecord(result);
+        return record;
     }
 
     @Override
-    public UserImgRecord uploadImage(UserImgRecord userImgRecord, MultipartFile file) throws IOException {
-        try (InputStream fileInputStream = file.getInputStream()) {
-            String contentType = file.getContentType();
-            String originalFileName = file.getOriginalFilename();
-            Long size = file.getSize();
-            log.info("ContentType: " + contentType);
-            log.info("OriginalFileName: " + originalFileName);
-            log.info("Size: " + size);
+    public String createPresignedUrlForGetImage(String key) {
+        try (S3Presigner s3Presigner = S3Presigner.create()) {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucket.name())
+                    .key(key)
+                    .build();
 
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType(contentType);
-            metadata.setContentLength(size);
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .getObjectRequest(getObjectRequest)
+                    .signatureDuration(Duration.ofMinutes(1))
+                    .build();
 
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket.getName(), originalFileName, fileInputStream, metadata);
+            PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(presignRequest);
 
-            PutObjectResult putObjectResult = s3Client.putObject(putObjectRequest);
-            boolean uploadResult = putObjectResult.getMetadata().getBucketKeyEnabled();
+            String url = presignedRequest.url().toString();
+            return url;
         }
-        return null;
+    }
+
+    @Override
+    @Transactional
+    public UserImgRecord uploadImage(UserRecord user, MultipartFile file) throws IOException {
+        try (InputStream fileInputStream = file.getInputStream()) {
+            String uid = user.uid();
+            String key = new StringBuilder(uid)
+                    .append("-")
+                    .append(file.getOriginalFilename())
+                    .toString();
+            String contentType = file.getContentType();
+            long size = file.getSize();
+            log.info("key: " + key);
+
+            UserImg userImg = UserImg.builder()
+                    .user(user.userId())
+                    .url(key)
+                    .contentType(contentType)
+                    .length(size)
+                    .build();
+
+            UserImg result = userImgRepository.save(userImg);
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket.name())
+                    .key(result.getUrl())
+                    .contentType(result.getContentType())
+                    .contentLength(result.getLength())
+                    .build();
+
+            RequestBody requestBody = RequestBody.fromInputStream(fileInputStream, result.getLength());
+
+            s3Client.putObject(putObjectRequest, requestBody);
+
+            return new UserImgRecord(result);
+        }
     }
 }
