4d8d1781467f41fb4021d741a8d2ed1459a7159b
신문영 <ztrl@naver.com>
2024-01-25 11:07:44 +0900

    feat: Add NoSuchElementException handler

--- src/main/java/com/example/ssafy/petcong/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ssafy/petcong/exception/GlobalExceptionHandler.java
@@ -7,13 +7,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+        log.warn(ex.getMessage());
+        return ResponseEntity
+                .internalServerError()
+                .body(ex.getMessage());
+    }
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
