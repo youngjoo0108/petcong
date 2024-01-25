f55a419deb7e068c8730f3728cf70f56664ccc26
신문영 <ztrl@naver.com>
2024-01-25 11:12:33 +0900

    feat: Add trick and withdraw api to UserController

--- src/main/java/com/example/ssafy/petcong/user/controller/UserController.java
+++ src/main/java/com/example/ssafy/petcong/user/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.example.ssafy.petcong.user.controller;
 
+import com.example.ssafy.petcong.user.model.record.SkillMultimediaRecord;
 import com.example.ssafy.petcong.user.model.record.UserImgRecord;
 import com.example.ssafy.petcong.user.model.record.UserRecord;
 import com.example.ssafy.petcong.user.service.UserService;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 
 import jakarta.validation.Valid;
 
@@ -65,35 +63,74 @@ public class UserController {
                     .body("No user founded.");
         }
     }
-    @ApiOperation(value = "프로필 이미지 url 얻기", notes = "생성된 presigned url로 이미지를 업로드")
+
+    @ApiOperation(value = "프로필 이미지 url 얻기", notes = "생성된 presigned url 다운로드 링크 제공")
     @ApiResponses({
             @ApiResponse(code = 200, message = "url 생성 성공")
     })
-    @GetMapping("/picture")
-    public ResponseEntity<?> getProfileImageUrl(String key) {
-        String url = userService.createPresignedUrlForGetImage(key);
+    @GetMapping({"/picture", "/trick"})
+    public ResponseEntity<?> getMediaUrl(String key) {
+        String url = userService.createPresignedUrl(key);
         return ResponseEntity
                 .ok()
                 .body(url);
     }
 
-    @ApiOperation(value = "프로필 이미지 업로드", notes = "이미지 업로드")
+    @ApiOperation(value = "프로필 이미지 업로드", notes = "이미지 파일은 S3, 메타데이터는 MySQL에 업로드")
     @ApiResponses({
             @ApiResponse(code = 200, message = "업로드 성공")
     })
     @PostMapping("/picture")
     public ResponseEntity<?> postProfileImage(@RequestBody String uid, @RequestParam("file")MultipartFile file) throws IOException {
-//        String uid = (String) SecurityContextHolder.getContext().getAuthentication().getCredentials();
+        //String uid = (String) SecurityContextHolder.getContext().getAuthentication().getCredentials();
 
         UserRecord user = userService.findUserByUid(uid);
 
-        UserImgRecord userImgRecord = userService.uploadImage(user, file);
+        UserImgRecord userImgRecord = userService.uploadUserImage(user, file);
 
         return ResponseEntity
                 .ok()
                 .body(userImgRecord);
     }
 
+    @ApiOperation(value = "개 인기 업로드", notes = "멀티미디어 파일은 S3, 메타데이터는 MySQL에 업로드")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "업로드 성공")
+    })
+    @PostMapping("/trick")
+    public ResponseEntity<?> postDogTrick(@RequestBody String uid, @RequestParam("file")MultipartFile file) throws IOException {
+        UserRecord user = userService.findUserByUid(uid);
 
+        SkillMultimediaRecord skillMultimediaRecord = userService.uploadSkillMultimedia(user, file);
 
+        return ResponseEntity
+                .ok()
+                .body(skillMultimediaRecord);
+    }
+
+    @ApiOperation(value = "회원탈퇴", notes = "회원탈퇴")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "탈퇴 성공"),
+            @ApiResponse(code = 202, message = "이미 탈퇴"),
+            @ApiResponse(code = 500, message = "탈퇴 실패")
+    })
+    @DeleteMapping("/withdraw")
+    public ResponseEntity<?> deleteUser(@RequestBody int userId) {
+        int deletedCount = userService.deleteUserByUserId(userId);
+
+        if (deletedCount == 1) {
+            return ResponseEntity
+                    .ok()
+                    .body("Successfully withdraw");
+        } else if (deletedCount == 0) {
+            return ResponseEntity
+                    .accepted()
+                    .body("Already withdraw");
+        }
+        else {
+            return ResponseEntity
+                    .internalServerError()
+                    .body("Not withdraw");
+        }
+    }
 }
