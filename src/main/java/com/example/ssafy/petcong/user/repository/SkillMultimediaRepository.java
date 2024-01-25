048f83ddf27b4402e90c509c2a3d390dae5a670d
신문영 <ztrl@naver.com>
2024-01-25 11:06:03 +0900

    feat: Add Multimedia domain

---
+++ src/main/java/com/example/ssafy/petcong/user/repository/SkillMultimediaRepository.java
@@ -0,0 +1,8 @@
+package com.example.ssafy.petcong.user.repository;
+
+import com.example.ssafy.petcong.user.model.entity.SkillMultimedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillMultimediaRepository extends JpaRepository<SkillMultimedia, Integer> {
+    SkillMultimedia save(SkillMultimedia skillMultimedia);
+}
