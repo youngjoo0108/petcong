ef1a02dd503c66f9393f0647b1c1e879ca4c2b6a
신문영 <ztrl@naver.com>
2024-01-29 14:07:32 +0900

    refactor: Refactor UserRepository

--- server_petcong/src/main/java/com/example/ssafy/petcong/user/repository/UserRepository.java
+++ server_petcong/src/main/java/com/example/ssafy/petcong/user/repository/UserRepository.java
@@ -5,7 +5,6 @@ import com.example.ssafy.petcong.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    int findUserIdByUid(String uid);
     User findUserByUserId(int userId);
     User findUserByUid(String uid);
     User save(User user);
