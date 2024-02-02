a4bb01325ae541305a5994f27b98148cb6c77f94
신문영 <ztrl@naver.com>
2024-02-02 17:59:27 +0900

    feat: Add SignupRequestDto

---
+++ server_petcong/src/main/java/com/example/ssafy/petcong/user/model/dto/SignupRequestDto.java
@@ -0,0 +1,27 @@
+package com.example.ssafy.petcong.user.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Schema(title = "회원가입 Request", description = "회원가입 요청 시 받는 Request")
+public class SignupRequestDto {
+    @Valid
+    @NotNull
+    @Schema(title = "유저 정보")
+    UserInfoDto userInfo;
+    @Valid
+    @NotNull
+    @Schema(title = "펫 정보")
+    PetInfoDto petInfo;
+}
