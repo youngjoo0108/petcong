9634f1da64d8d6121bf1b444c5ce333a6b699a9e
jongwoo <jongwoop97@gmail.com>
2024-02-02 16:04:32 +0900

    feat: get users/info request and post users/picture request

--- client_petcong/lib/services/user_service.dart
+++ client_petcong/lib/services/user_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:petcong/controller/user_controller.dart';
 import 'package:petcong/models/user_model.dart';
@@ -9,37 +10,64 @@ const bool testing = true;
 
 User? currentUser = UserController.currentUser;
 String idTokenString = currentUser?.getIdToken().toString() ?? "";
+const String serverUrl = 'https://i10a603.p.ssafy.io';
 Map<String, String> reqHeaders = checkTesting();
 
-Future<UserModel> fetchAlbum() async {
+// GET /users/info
+Future<UserModel> getUserInfo() async {
   // ---- local testing
   // final response = await http.get(Uri.parse('http://10.0.2.2:8080/users/info'),
   //     headers: {"tester": "A603"});
 
   // ---- server testing
-  final response = await http.get(
-      Uri.parse('http://i10a603.p.ssafy.io:8080/users/info'),
-      headers: reqHeaders);
+  final response =
+      await http.get(Uri.parse('$serverUrl/users/info'), headers: reqHeaders);
 
   if (response.statusCode == 200) {
     return UserModel.fromJson(
         jsonDecode(response.body) as Map<String, dynamic>);
   } else {
-    print(response.statusCode);
-    print(response.body);
-    print("error");
-    throw Exception('Failed to load album.');
+    if (kDebugMode) {
+      print(response.statusCode);
+      print(response.body);
+      print("error");
+    }
+    throw Exception("user doesn't exist.");
   }
 }
 
-Map<String, String> checkTesting() {
-  if (testing == true) {
-    return {
-      'tester': 'A603',
-    };
-  } else {
-    return {
-      'Petcong-id-token': idTokenString,
-    };
+Future<void> postPicture(List<String> filePaths) async {
+  const String endpoint = '$serverUrl/users/picture';
+  var request = http.MultipartRequest('POST', Uri.parse(endpoint));
+
+  for (String filePath in filePaths) {
+    var file = await http.MultipartFile.fromPath('files', filePath);
+    request.files.add(file);
   }
+
+  try {
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      if (kDebugMode) {
+        print("success");
+      }
+    } else if (kDebugMode) {
+      print("failed");
+      print(response.statusCode);
+      print(response.stream.bytesToString());
+    }
+  } catch (error) {
+    if (kDebugMode) {
+      print("error");
+      print(error.toString());
+    }
+  }
+}
+
+Future<void> patchPicture(List<String> keys) async {
+  Map<String, String> jsonBody = {'keys': keys.toString()};
+}
+
+Map<String, String> checkTesting() {
+  return testing ? {'tester': 'A603'} : {'Petcong-id-token': idTokenString};
 }
