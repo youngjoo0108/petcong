907e1cd0d174d72d063c65cf3c6be1e77bd52f86
신문영 <ztrl@naver.com>
2024-02-02 12:40:50 +0900

    feat: Add OpenAPIDefinition for swagger api test

--- server_petcong/src/main/java/com/example/ssafy/petcong/PetcongApplication.java
+++ server_petcong/src/main/java/com/example/ssafy/petcong/PetcongApplication.java
@@ -1,11 +1,15 @@
 package com.example.ssafy.petcong;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+        @Server(url = "/", description = "Default Server URL")
+})
 public class PetcongApplication {
 
     public static void main(String[] args) {
