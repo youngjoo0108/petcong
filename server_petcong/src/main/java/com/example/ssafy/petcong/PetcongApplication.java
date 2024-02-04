cb915da4a94a1b0cde8acbfa1ed2ff407895f78a
SMY <ztr@naver.com>
2024-02-04 20:45:32 +0900

    refactor: Move OpenAPIDefinition server config to SpringdocConfig

--- server_petcong/src/main/java/com/example/ssafy/petcong/PetcongApplication.java
+++ server_petcong/src/main/java/com/example/ssafy/petcong/PetcongApplication.java
@@ -1,15 +1,9 @@
 package com.example.ssafy.petcong;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(servers = {
-        @Server(url = "/", description = "Default Server URL")
-})
 public class PetcongApplication {
 
     public static void main(String[] args) {
