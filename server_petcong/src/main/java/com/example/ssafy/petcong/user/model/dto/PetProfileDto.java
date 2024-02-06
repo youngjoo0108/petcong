99fdba6141a74bc0d4ad12365c65103e583bc4a5
신문영 <ztrl@naver.com>
2024-02-06 12:37:50 +0900

    feat: Add ProfileDto to reponse /users/info

---
+++ server_petcong/src/main/java/com/example/ssafy/petcong/user/model/dto/PetProfileDto.java
@@ -0,0 +1,20 @@
+package com.example.ssafy.petcong.user.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@Schema(title = "펫 프로필 정보", description = "펫 프로필 사진은 여러장 일 수 있음")
+public class PetProfileDto {
+    PetInfoDto petInfo;
+    List<SkillMultimediaInfoDto> skillMultimediaInfoList;
+}
