d570d1291d75e8580fa24547922e27daf9e97f9c
신문영 <ztrl@naver.com>
2024-02-07 09:22:47 +0900

    refactor: Refactor user

--- server_petcong/src/main/java/com/example/ssafy/petcong/matching/service/MatchingProfileService.java
+++ server_petcong/src/main/java/com/example/ssafy/petcong/matching/service/MatchingProfileService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MatchingProfileService {
-    List<String> pictures(int userId);
+    List<String> pictures(int memberId);
     Optional<ProfileRecord> profile(String uid);
     List<Matching> findMatchingList(int fromMemberId, int toMemberId);
 }
