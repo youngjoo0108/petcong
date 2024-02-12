07f267997c555a98e13027a164828c00175339b6
paul <cebu13@gmail.com>
2024-02-12 20:48:50 +0900

    feat: simple history card

--- client_petcong/lib/pages/app_pages/chat/chat_page.dart
+++ client_petcong/lib/pages/app_pages/chat/chat_page.dart
@@ -10,7 +10,7 @@ class MainChatPage extends StatefulWidget {
 }
 
 class _MainChatPageState extends State<MainChatPage> {
-  int itemsCount = 20;
+  int itemsCount = profiles.length;
   @override
   void initState() {
     Future.delayed(const Duration(milliseconds: 500) * 5, () {
@@ -18,7 +18,7 @@ class _MainChatPageState extends State<MainChatPage> {
         return;
       }
       setState(() {
-        itemsCount += 10;
+        itemsCount += 6;
       });
     });
     super.initState();
@@ -28,19 +28,97 @@ class _MainChatPageState extends State<MainChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: LiveGrid(
-          padding: const EdgeInsets.all(1),
-          showItemInterval: const Duration(milliseconds: 50),
-          showItemDuration: const Duration(milliseconds: 150),
-          visibleFraction: 0.001,
-          itemCount: itemsCount,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 2,
-            mainAxisSpacing: 2,
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 0.8, // Adjust this value as needed
+          children: List.generate(profiles.length, (index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) =>
+                            ProfileDetailPage(profile: profiles[index])));
+              },
+              child: ProfileCard(profile: profiles[index]),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+class Profile {
+  final String name;
+  final String description;
+  final String imageUrl;
+  final String instagram;
+  final String kakaoId;
+
+  Profile(
+      this.name, this.description, this.imageUrl, this.instagram, this.kakaoId);
+}
+
+List<Profile> profiles = [
+  Profile(
+      'Name1', 'Description1', 'assets/src/test_1.jpg', 'instagram1', 'kakao1'),
+  Profile(
+      'Name2', 'Description2', 'assets/src/test_5.jpg', 'instagram2', 'kakao2'),
+];
+
+class ProfileCard extends StatelessWidget {
+  final Profile profile;
+
+  const ProfileCard({Key? key, required this.profile}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Hero(
+            tag: profile.imageUrl,
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Image.asset(profile.imageUrl, fit: BoxFit.cover),
+            ),
           ),
-          itemBuilder: animationItemBuilder(
-              (index) => HorizontalItem(title: index.toString())),
+          Text(profile.name, style: const TextStyle(fontSize: 16)),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileDetailPage extends StatelessWidget {
+  final Profile profile;
+
+  const ProfileDetailPage({Key? key, required this.profile}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(profile.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Hero(
+              tag: profile.imageUrl,
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Image.asset(profile.imageUrl, fit: BoxFit.cover),
+              ),
+            ),
+            Text(profile.name, style: const TextStyle(fontSize: 24)),
+            const SizedBox(height: 10),
+            Text(profile.description),
+            Text('Instagram: ${profile.instagram}'),
+            Text('Kakao ID: ${profile.kakaoId}'),
+          ],
         ),
       ),
     );
