b94e16563e92ad86f07471015e3ccc542f4c31c2
jujaewon <lahmthebest@naver.com>
2024-02-12 21:26:15 +0900

    second quiz

--- client_petcong/lib/pages/app_pages/quiz/second_quiz.dart
+++ client_petcong/lib/pages/app_pages/quiz/second_quiz.dart
@@ -2,28 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:petcong/widgets/continue_button.dart';
 import 'quiz.dart';
+import 'third_quiz.dart'; // ThirdQuizPage를 import하였습니다. 이동할 페이지에 맞게 변경해주세요.
 
-class SecondQuizPage extends StatelessWidget {
+class SecondQuizPage extends StatefulWidget {
+  const SecondQuizPage({super.key});
+
+  @override
+  SecondQuizPageState createState() => SecondQuizPageState();
+}
+
+class SecondQuizPageState extends State<SecondQuizPage> {
   final QuizManager quizManager = QuizManager();
+  bool _isButtonDisabled = true; // 버튼 활성화 상태를 저장하는 변수
 
-  SecondQuizPage({super.key}) {
-    quizManager.addQuiz(Quiz(question: '프랑스의 수도는?', answer: '파리'));
-    quizManager.addQuiz(Quiz(question: '일본의 수도는?', answer: '도쿄'));
+  SecondQuizPageState() {
+    quizManager.addQuiz(Quiz(question: '상대방 반려 동물의 나이는?', answer: ''));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Second Quiz')),
+      appBar: AppBar(title: const Text('두 번째 퀴즈')),
       body: ListView.builder(
         itemCount: quizManager.getQuizCount(),
         itemBuilder: (context, index) {
           Quiz quiz = quizManager.getQuiz(index);
 
           return ListTile(
-            title: Text('퀴즈: ${quiz.question}'),
+            title: Text('${quiz.question}의 정답은?'),
             subtitle: TextField(
               onChanged: (value) {
+                setState(() {
+                  _isButtonDisabled = value.isEmpty; // 입력이 있으면 버튼 활성화
+                });
                 if (value == quiz.answer) {
                   print('정답입니다!');
                 } else {
@@ -35,11 +46,13 @@ class SecondQuizPage extends StatelessWidget {
         },
       ),
       bottomNavigationBar: ContinueButton(
-        isFilled: true,
+        isFilled: !_isButtonDisabled,
         buttonText: 'CONTINUE',
-        onPressed: () {
-          Get.to(ThirdQuizPage());
-        },
+        onPressed: !_isButtonDisabled
+            ? () {
+                Get.to(const ThirdQuizPage()); // 다음 퀴즈 페이지로 이동
+              }
+            : null,
       ),
     );
   }
