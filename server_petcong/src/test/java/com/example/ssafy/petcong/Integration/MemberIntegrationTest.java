45a0a4db97192fa6e037774bf198a51d3f7bbf90
신문영 <ztrl@naver.com>
2024-02-13 12:43:26 +0900

    test: Add dummy members

--- server_petcong/src/test/java/com/example/ssafy/petcong/Integration/MemberIntegrationTest.java
+++ server_petcong/src/test/java/com/example/ssafy/petcong/Integration/MemberIntegrationTest.java
@@ -36,6 +36,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -48,62 +51,170 @@ public class MemberIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private static final int SIZE = 100;
+
+    private static final String[] hobbies = {
+            "산책", "원반던지기", "수영", "뒹굴기", "아무것도 안하기", "개껌 씹기"
+    };
+
+    private static final String[] kinds = {
+            "골든 리트리버", "치와와", "도베르만", "진돗개", "시츄", "삽살개", "닥스훈트", "보더콜리", "허스키", "셰퍼드"
+    };
+
+    private static final String[] names = {
+            "뽀삐",
+            "멍멍이",
+            "야옹이",
+            "멈뭄미",
+            "초코",
+            "보리",
+            "엘살바도르",
+            "제우스",
+            "토르",
+            "라미엘",
+            "울라리",
+            "제라드",
+            "훔바훔바",
+            "우유",
+            "개호주",
+            "페리시치",
+            "손흥민",
+            "김민재",
+            "이강인",
+            "메시",
+            "호날두",
+            "즐라탄",
+            "페이커",
+            "구마유시",
+            "대상혁",
+            "아르키메데스",
+            "플라톤",
+            "니체",
+            "루돌프",
+            "이소룡",
+            "딸기",
+            "아기",
+            "묠니르",
+            "티모",
+            "오딘",
+            "알아키르",
+            "피즈",
+            "럼블",
+            "호랑이",
+            "늑대",
+            "사자",
+            "재규어",
+            "치타",
+            "표범"
+    };
+
+    private static final PetSize[] sizes = {
+            PetSize.SMALL, PetSize.MEDIUM, PetSize.LARGE
+    };
+
+    private static final String[] toys = {
+            "개껌", "원반", "쥐방울", "공", "포인터"
+    };
+
+    private static final String[] descriptions = {
+            "저희 개는 안물어요",
+            "개팔자가 상팔자",
+            "세상에 나쁜 개는 없다",
+            "산책 안가면 죽는 병에 걸림",
+            "개를 위한 나라는 없다",
+            "바르개 살자",
+            "빠지는 털로 목도리를 짤 수 있습니다",
+            "배를 만져주면 좋아합니다",
+            "혼낼 때 세상 억울한 표정 지음",
+            "개들을 풀어라"
+    };
+
+    private static final String[] snacks = {
+            "생닭",
+            "참치캔",
+            "스팸",
+            "닭가슴살",
+            "츄르",
+            "오리목뼈",
+            "족발",
+            "소세지",
+            "수육",
+            "스테이크",
+            "토마호크",
+            "케비아",
+            "사과",
+            "바나나",
+            "고구마",
+            "육포",
+            "먹태",
+            "햄"
+    };
+
     static Stream<Arguments> provideDummySignUpMember() {
-        SignupMemberInfoDto signupMemberInfoDto = new SignupMemberInfoDto();
-        signupMemberInfoDto.setAge(10);
-        signupMemberInfoDto.setNickname("nickname");
-        signupMemberInfoDto.setEmail("signuptest@signuptest.com");
-        signupMemberInfoDto.setAddress("korea");
-        signupMemberInfoDto.setInstagramId("instatonid");
-        signupMemberInfoDto.setKakaoId("kakaochocolate");
-        signupMemberInfoDto.setBirthday(LocalDate.of(1997, 1, 29));
-        signupMemberInfoDto.setGender(Gender.MALE);
-        signupMemberInfoDto.setPreference(Preference.FEMALE);
+        List<SignupRequestDto> signupRequestDtos = new ArrayList<>();
+        for (int i = 0; i < SIZE; i++) {
+            SignupMemberInfoDto signupMemberInfoDto = new SignupMemberInfoDto();
+            signupMemberInfoDto.setAge(new Random().nextInt(20, 31));
+            signupMemberInfoDto.setNickname("닉네임" + new Random().nextInt(0, Integer.MAX_VALUE));
+            signupMemberInfoDto.setEmail("test" + new Random().nextInt(0, 10_000) +"@petcong.com");
+            signupMemberInfoDto.setAddress("서울시 강남구");
+            signupMemberInfoDto.setInstagramId("insta_" + new Random().nextInt(0, Integer.MAX_VALUE));
+            signupMemberInfoDto.setKakaoId("kakako_" + new Random().nextInt(0, Integer.MAX_VALUE));
+            signupMemberInfoDto.setBirthday(LocalDate.of(new Random().nextInt(1990, 2000), new Random().nextInt(1, 12), new Random().nextInt(1, 29)));
+            signupMemberInfoDto.setGender(new Random().nextBoolean() ? Gender.MALE : Gender.FEMALE);
+            signupMemberInfoDto.setPreference(new Random().nextBoolean() ? Preference.MALE : Preference.FEMALE);
 
-        PetInfoDto petInfoDto = new PetInfoDto();
-        petInfoDto.setAge(1);
-        petInfoDto.setGender(Gender.FEMALE);
-        petInfoDto.setDbti("INFP");
-        petInfoDto.setHobby("산책");
-        petInfoDto.setBreed("골드 리트리버");
-        petInfoDto.setName("야옹이");
-        petInfoDto.setSize(PetSize.LARGE);
-        petInfoDto.setToy("원반");
-        petInfoDto.setDescription("우리 개는 물어요.");
-        petInfoDto.setNeutered(false);
-        petInfoDto.setWeight(30);
-        petInfoDto.setSnack("생닭다리");
+            PetInfoDto petInfoDto = new PetInfoDto();
+            petInfoDto.setAge(new Random().nextInt(1, 20));
+            petInfoDto.setGender(new Random().nextBoolean() ? Gender.MALE : Gender.FEMALE);
+            petInfoDto.setDbti((new Random().nextBoolean() ? "E" : "I") +
+                    (new Random().nextBoolean() ? "N" : "S") +
+                    (new Random().nextBoolean() ? "F" : "T") +
+                    (new Random().nextBoolean() ? "P" : "J"));
+            petInfoDto.setHobby(hobbies[new Random().nextInt(hobbies.length)]);
+            petInfoDto.setBreed(kinds[new Random().nextInt(kinds.length)]);
+            petInfoDto.setName(names[new Random().nextInt(names.length)]);
+            petInfoDto.setSize(sizes[new Random().nextInt(sizes.length)]);
+            petInfoDto.setToy(toys[new Random().nextInt(toys.length)]);
+            petInfoDto.setDescription(descriptions[new Random().nextInt(descriptions.length)]);
+            petInfoDto.setNeutered(new Random().nextBoolean());
+            petInfoDto.setWeight(new Random().nextInt(9, 20));
+            petInfoDto.setSnack(snacks[new Random().nextInt(snacks.length)]);
 
-        SignupRequestDto signupRequestDto = new SignupRequestDto(signupMemberInfoDto, petInfoDto);
-        return Stream.of(Arguments.of(signupRequestDto));
+            signupRequestDtos.add(new SignupRequestDto(signupMemberInfoDto, petInfoDto));
+        }
+
+        return Stream.of(Arguments.of(signupRequestDtos));
     }
 
     @ParameterizedTest
     @MethodSource("provideDummySignUpMember")
     @DisplayName("SignUp Test")
-    void testSignUp(SignupRequestDto signupRequestDto) throws Exception {
-        //given
-        String memberRecordJson = objectMapper.writeValueAsString(signupRequestDto);
+    void testSignUp(List<SignupRequestDto> signupRequestDtos) throws Exception {
+        for (SignupRequestDto signupRequestDto : signupRequestDtos) {
+            //given
+            String memberRecordJson = objectMapper.writeValueAsString(signupRequestDto);
 
-        //when
-        var request = MockMvcRequestBuilders
-                .post("/members/signup")
-                .header("tester", "A603")
-                .content(memberRecordJson)
-                .contentType(MediaType.APPLICATION_JSON);
+            //when
+            var request = MockMvcRequestBuilders
+                    .post("/members/signup")
+                    .content(memberRecordJson)
+                    .param("uid", String.valueOf(new Random().nextInt(1_000_000_000, 2_000_000_001)))
+                    .contentType(MediaType.APPLICATION_JSON);
 
-        //then
-        MvcResult mvcResult = mockMvc
-                .perform(request)
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andReturn();
+            //then
+            MvcResult mvcResult = mockMvc
+                    .perform(request)
+                    .andExpect(status().isCreated())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andReturn();
 
-        String response = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+            String response = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        assertThat(response).isNotNull();
+            assertThat(response).isNotNull();
 
-        log.info("SignUp Test: " + response);
+            log.info("SignUp Test: " + response);
+        }
     }
 
     @Test
