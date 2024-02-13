011e56a8d20cdcb2178532a464bc04bd959bbc0c
신문영 <ztrl@naver.com>
2024-02-13 17:07:47 +0900

    fix: Fix Member lazy fetch type in Matching to eager fetch type

--- server_petcong/src/main/java/com/example/ssafy/petcong/matching/model/entity/Matching.java
+++ server_petcong/src/main/java/com/example/ssafy/petcong/matching/model/entity/Matching.java
@@ -30,11 +30,11 @@ public class Matching {
     @Column(name = "matching_id")
     private int id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(referencedColumnName = "member_id", name = "from_member_id")
     private Member fromMember;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(referencedColumnName = "member_id", name = "to_member_id")
     private Member toMember;
 
