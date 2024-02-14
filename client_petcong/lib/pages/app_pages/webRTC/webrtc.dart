540cd6e8b39686dc8893b1d34c89213a14607545
youngjoo <youngjoo000108@gmail.com>
2024-02-14 15:51:57 +0900

    fixed moving webrtc local screen

--- client_petcong/lib/pages/app_pages/webRTC/webrtc.dart
+++ client_petcong/lib/pages/app_pages/webRTC/webrtc.dart
@@ -28,9 +28,13 @@ class _MainVideoCallWidgetState extends State<MainVideoCallWidget> {
   late double scaleValue = 0.5;
   late double localRendererX = videoWidth * (6 / 7);
   late double localRendererY = videoHeight / 20;
-
+  // late double test = 0;
   // icebreakings
-  List<String> quizs = ["sampleQuiz1", "sampleQuiz2", "sampleQuiz3"];
+  List<String> quizs = [
+    "똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중똥싸는중",
+    "sampleQuiz2",
+    "sampleQuiz3"
+  ];
   bool showMessage = false;
 
   void _toggleMessageDialog() {
@@ -50,17 +54,33 @@ class _MainVideoCallWidgetState extends State<MainVideoCallWidget> {
     super.dispose();
   }
 
-  void onIdxbtnPressed() {
-    int maxIdx = quizs.length;
-    if (MainVideoCallWidget.quizIdx >= maxIdx) {
-      MainVideoCallWidget.quizIdx = maxIdx;
-      print("===============index changed by me / max!!");
-      return;
-    }
-    MainVideoCallWidget.quizIdx++;
-    print(
-        "===============index changed by me / index = ${MainVideoCallWidget.quizIdx}==");
-    SocketService.sendMessage("idx", MainVideoCallWidget.quizIdx.toString());
+  void onIdxPlusBtnPressed() {
+    setState(() {
+      int maxIdx = quizs.length;
+      if (MainVideoCallWidget.quizIdx >= maxIdx) {
+        MainVideoCallWidget.quizIdx = maxIdx;
+        print("===============index changed by me / max!!");
+        return;
+      }
+      MainVideoCallWidget.quizIdx++;
+      print(
+          "===============index changed by me / index = ${MainVideoCallWidget.quizIdx}==");
+      SocketService.sendMessage("idx", MainVideoCallWidget.quizIdx.toString());
+    });
+  }
+
+  void onIdxMinusBtnPressed() {
+    setState(() {
+      if (MainVideoCallWidget.quizIdx <= 0) {
+        MainVideoCallWidget.quizIdx = 0;
+        print("===============index changed by me / max!!");
+        return;
+      }
+      MainVideoCallWidget.quizIdx--;
+      print(
+          "===============index changed by me / index = ${MainVideoCallWidget.quizIdx}==");
+      SocketService.sendMessage("idx", MainVideoCallWidget.quizIdx.toString());
+    });
   }
 
   @override
@@ -70,101 +90,152 @@ class _MainVideoCallWidgetState extends State<MainVideoCallWidget> {
       ..scale(scaleValue)
       ..translate(localRendererX, localRendererY);
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          // 상대방 화면
-          SizedBox.expand(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: RTCVideoView(
-                widget.remoteRenderer,
-                mirror: false,
+      body: GestureDetector(
+        onTapDown: (position) {
+          print(position.globalPosition);
+        },
+        child: Stack(
+          children: <Widget>[
+            // 상대방 화면
+            SizedBox.expand(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: RTCVideoView(
+                  widget.remoteRenderer,
+                  mirror: false,
+                ),
               ),
             ),
-          ),
-          //  내 화면
-          // ClipRect(
-          //   child: InteractiveViewer(
-          //     transformationController: controller,
-          //     minScale: 0.3,
-          //     maxScale: 0.5,
-          //     constrained: true,
-          //     boundaryMargin: const EdgeInsets.all(double.infinity),
-          //     child: SizedBox(
-          //       width: videoWidth,
-          //       height: videoHeight,
-          //       child: ClipRRect(
-          //         borderRadius: BorderRadius.circular(20),
-          //         child: RTCVideoView(
-          //           widget.localRenderer,
-          //           objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
-          //           mirror: true,
-          //         ),
-          // 내 화면
-          ClipRect(
-            child: InteractiveViewer(
-              transformationController: controller,
-              minScale: 0.3,
-              maxScale: 0.5,
-              constrained: true,
-              boundaryMargin: const EdgeInsets.all(double.infinity),
-              onInteractionUpdate: ((details) {
-                // setState(() {});
-                localRendererX = details.focalPointDelta.dx;
-                localRendererY = details.focalPointDelta.dy;
-                scaleValue = details.scale;
-                print(scaleValue);
-              }),
-              child: SizedBox(
-                width: videoWidth,
-                height: videoHeight,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: RTCVideoView(
-                    widget.localRenderer,
-                    objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
-                    mirror: true,
+            //  내 화면
+            ClipRect(
+              child: InteractiveViewer(
+                transformationController: controller,
+                minScale: 0.2,
+                maxScale: 0.5,
+                constrained: true,
+                boundaryMargin: const EdgeInsets.all(double.infinity),
+                onInteractionStart: (details) {},
+                onInteractionUpdate: (details) {
+                  scaleValue = controller.value.getMaxScaleOnAxis();
+                  localRendererX =
+                      controller.value.getTranslation().x / scaleValue;
+                  localRendererY =
+                      controller.value.getTranslation().y / scaleValue;
+                },
+                child: SizedBox(
+                  width: videoWidth,
+                  height: videoHeight,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: RTCVideoView(
+                      widget.localRenderer,
+                      objectFit:
+                          RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                      mirror: true,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-      // 통화 종료 버튼
-      floatingActionButton: Row(
+      floatingActionButton: Column(
         children: [
-          FloatingActionButton(
-            onPressed: () async {
-              widget.localRenderer.srcObject!.getTracks().forEach((track) {
-                track.stop();
-              });
-              // disconnectCall 로직
-              await widget.localRenderer.srcObject!.dispose();
-              await widget.pc!.close();
-              widget.pc = null;
-              print(
-                  "end btn.onPressed - localRederer.hashCode = ${widget.localRenderer.hashCode}");
-              print(
-                  "end btn.onPressed - remoteRenderer.hashCode = ${widget.remoteRenderer.hashCode}");
-              await widget.localRenderer.dispose();
-              await widget.remoteRenderer.dispose();
-              // disconnect end
-              SocketService().setCallPressed(false); // flag false로
-              await Future.delayed(const Duration(seconds: 2));
-              Get.offAll(const HomePage());
-            },
-            heroTag: 'stop_call_button',
-            shape: const CircleBorder(eccentricity: 0),
-            backgroundColor: MyColor.petCongColor4,
-            child: const Icon(Icons.call_end),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 100),
+                      width: showMessage
+                          ? MediaQuery.of(context).size.width - videoWidth / 3
+                          : 0.0,
+                      height: showMessage ? 50.0 : 0.0,
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: showMessage
+                          ? Container(
+                              color: Colors.white.withOpacity(0.3),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                    onPressed: onIdxMinusBtnPressed,
+                                    icon: const Icon(
+                                        Icons.arrow_back_ios_new_rounded),
+                                  ),
+                                  Text(
+                                    quizs.isNotEmpty &&
+                                            MainVideoCallWidget.quizIdx >= 0 &&
+                                            MainVideoCallWidget.quizIdx <
+                                                quizs.length
+                                        ? quizs[MainVideoCallWidget.quizIdx]
+                                        : 'No quiz available',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: onIdxPlusBtnPressed,
+                                    icon: const Icon(
+                                        Icons.arrow_forward_ios_rounded),
+                                  ),
+                                ],
+                              ),
+                            )
+                          : null,
+                    ),
+                    Opacity(
+                      opacity: showMessage ? 1.0 : 0.2,
+                      child: FloatingActionButton.small(
+                        onPressed: _toggleMessageDialog,
+                        heroTag: 'text',
+                        child: const Icon(Icons.message),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          FloatingActionButton(
-            onPressed: onIdxbtnPressed,
-            shape: const CircleBorder(eccentricity: 0),
-            backgroundColor: Colors.blue,
-            heroTag: 'next_button',
-            child: const Icon(Icons.call_end),
+          // 통화 종료 버튼
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: FloatingActionButton(
+              onPressed: () async {
+                widget.localRenderer.srcObject!.getTracks().forEach((track) {
+                  track.stop();
+                });
+                // disconnectCall 로직
+                await widget.localRenderer.srcObject!.dispose();
+                await widget.pc!.close();
+                widget.pc = null;
+                print(
+                    "end btn.onPressed - localRederer.hashCode = ${widget.localRenderer.hashCode}");
+                print(
+                    "end btn.onPressed - remoteRenderer.hashCode = ${widget.remoteRenderer.hashCode}");
+                await widget.localRenderer.dispose();
+                await widget.remoteRenderer.dispose();
+                // disconnect end
+                SocketService().setCallPressed(false); // flag false로
+                await Future.delayed(const Duration(seconds: 2));
+                Get.offAll(const HomePage());
+              },
+              heroTag: 'stop_call_button',
+              shape: const CircleBorder(),
+              backgroundColor: MyColor.petCongColor4,
+              elevation: 5,
+              child: const Icon(Icons.call_end),
+            ),
           ),
         ],
       ),
@@ -172,3 +243,41 @@ class _MainVideoCallWidgetState extends State<MainVideoCallWidget> {
     );
   }
 }
+
+// floatingActionButton: Row(
+//   children: [
+//     FloatingActionButton(
+//       onPressed: () async {
+//         widget.localRenderer.srcObject!.getTracks().forEach((track) {
+//           track.stop();
+//         });
+//         // disconnectCall 로직
+//         await widget.localRenderer.srcObject!.dispose();
+//         await widget.pc!.close();
+//         widget.pc = null;
+//         print(
+//             "end btn.onPressed - localRederer.hashCode = ${widget.localRenderer.hashCode}");
+//         print(
+//             "end btn.onPressed - remoteRenderer.hashCode = ${widget.remoteRenderer.hashCode}");
+//         await widget.localRenderer.dispose();
+//         await widget.remoteRenderer.dispose();
+//         // disconnect end
+//         SocketService().setCallPressed(false); // flag false로
+//         await Future.delayed(const Duration(seconds: 2));
+//         Get.offAll(const HomePage());
+//       },
+//       heroTag: 'stop_call_button',
+//       shape: const CircleBorder(eccentricity: 0),
+//       backgroundColor: MyColor.petCongColor4,
+//       child: const Icon(Icons.call_end),
+//     ),
+//     FloatingActionButton(
+//       onPressed: onIdxbtnPressed,
+//       shape: const CircleBorder(eccentricity: 0),
+//       backgroundColor: Colors.blue,
+//       heroTag: 'next_button',
+//       child: const Icon(Icons.call_end),
+//     ),
+//   ],
+// ),
+// floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
